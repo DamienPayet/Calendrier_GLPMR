148c7fedd2505cc4327dafc04e0f7734475fb297
damien PAYET <damien-payet@outlook.fr>
2020-06-26 08:14:29 +0200

    Generation seeder
    Alpha

--- app/Http/Controllers/TraitementController.php
+++ app/Http/Controllers/TraitementController.php
@@ -11,7 +11,84 @@ class TraitementController extends Controller
   public function index(){
     return view("back.traitement.index");
   }
-  public function seedergen(){
+  public function seedergene(Request $request){
+    function CSV(){
+      $counter = 0;
+      $counter2 = 13;
+      $counter5 = 0;
+      $number = 0;
+
+      if (($handle = fopen("csv/value.csv", "r")) !== FALSE) {
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+          $num = count($data);
+
+          $counter5++;
+          if ($counter5 > 1) {
+            $counter++;
+            switch ($counter) {
+              case 1:
+              $number = "1";
+              $counter2 = $counter2 - 1;
+              break;
+              case 2:
+              $number =  "2";
+              break;
+              case 3:
+              $number =  "4";
+              break;
+              case 4:
+              $number = "8";
+              $counter = 0;
+              break;
+            }
+            $counter3 = $counter2;
+            if ($counter2 > 0) {
+              do {
+                $number = "0" . $number;
+                $counter3 = $counter3 - 1 ;
+              } while ($counter3 != 0);
+              $val = 13 - $counter2 -1 ;
+              for ($i=0; $i < $val ; $i++) {
+                $number =  $number . "0";
+              }
+
+            }else{
+              $number = $number . "000000000000";
+            }
+            $line ="   DB::table('semaines')->insert([ " ."\r\n";
+            $line .=" 'nom'=> '". $number."', "."\r\n";
+            $line .= "'num'=> '".$number . "',"."\r\n";
+            $line .= "'numsem'=>'".$number."',"."\r\n";
+            $line .= "'debut'=>'".$number."',"."\r\n";
+            $line .= "'fin'=>'".$number."',"."\r\n";
+            $line .= "]);"."\r\n";
+            echo $line;
+          }
+        }
+        fclose($handle);
+      }
+    }
+    //Pour le CSV
+    $rand = "fsffds";
+    $CSV_upload = $request->file('file');
+
+    if($CSV_upload != NULL){
+      // Définition du chemin de stockage
+      // Nommage du fichier
+      $CSV_nommage = $CSV_upload->getClientOriginalName();
+      // On indique le chemin du fichier pour la base de donnée
+      $CSV_get = 'CSV\\' . $CSV_nommage;
+      // Si il y a bien un fichier, on le déplace dans le répertoire et on stock le chemin dans la base de donnée
+      if ($CSV_upload) {
+        if ($CSV_upload->move('csv', $CSV_nommage)) {
+          CSV();
+        }
+      } else {
+        // ENVOYER UNE ERREUR
+      }
+    }else {
+      //envoyer yne erreur
+    }
     return view("back.traitement.index");
   }
   public function traitementimages(){
--- resources/views/back/traitement/index.blade.php
+++ resources/views/back/traitement/index.blade.php
@@ -1,6 +1,16 @@
+<h1>Lancer traitement images</h1>
 <form method="post" action="{{route('traitementstart')}}">
   @csrf
   <div class="form-example">
+    <input type="submit" value="Lancer le traitement">
+  </div>
+</form>
+<h1>Générer seeder BDD</h1>
+<form method="post" action="{{route('traitementseeder')}}"  enctype="multipart/form-data" >
+  @csrf
+  <div class="form-example">
+    <input type="file" id="file" name="file">
+    <input type="text" id="txt" name="txt">
     <input type="submit" value="Lancer le traitement">
   </div>
 </form>
